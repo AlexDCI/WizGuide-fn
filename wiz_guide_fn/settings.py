ac77d126b9e89e07623a67b18eee6cd015050f7a
AlexDCI <oleksandr.hlukhoman@dci-student.org>
2025-02-18 14:22:14 +0100

    Update debug settings and configurations for production

--- wiz_guide_fn/settings.py
+++ wiz_guide_fn/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'  # Преобразуем строку в булевое значение
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['46.101.244.190', 'localhost', '127.0.0.1']
 
 
 # Application definition
