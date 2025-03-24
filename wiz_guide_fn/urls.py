c593b0be4e81ec46b0141156aa8c073cce1f931c
AlexDCI <oleksandr.hlukhoman@dci-student.org>
2025-03-24 15:04:04 +0100

    registranion via google

--- wiz_guide_fn/urls.py
+++ wiz_guide_fn/urls.py
@@ -17,20 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-<<<<<<< HEAD
-=======
 
->>>>>>> main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')), 
-    path('accounts/', include('allauth.urls')),
+    path('', include('main.urls')),
 
     path("users/", include("users.urls")),
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
-<<<<<<< HEAD
-=======
     path('accounts/', include('allauth.urls')),
->>>>>>> main
+
 ]
