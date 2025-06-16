8388a48725de2cfac7dbc4fee0d88c95762fd3e3
AlexDCI <oleksandr.hlukhoman@dci-student.org>
2025-06-16 16:54:30 +0200

    empruve frontend

--- wiz_guide_fn/urls.py
+++ wiz_guide_fn/urls.py
@@ -17,11 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), 
+    path('accounts/', include('allauth.urls')),
 
     path("users/", include("users.urls")),
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
