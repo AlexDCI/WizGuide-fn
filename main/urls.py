e9a66f9f6c2ea5c06128f1f1ef6dc255d38483a6
AlexDCI <oleksandr.hlukhoman@dci-student.org>
2025-09-12 19:04:07 +0200

    Fix FASTAPI_URL

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,20 @@
-# main/urls.py
-
 from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    # ПРОКСИ НА FASTAPI
+    # прокси
     re_path(r'^voice/(?P<path>.*)$', views.voice_proxy, name='voice_proxy'),
 
-    path('', views.main_page, name='main'),  # Главная страница
-    # path("chat/", views.chat_with_openai, name="chat_with_openai"),
+    # алиасы под фронтовые пути
+    path('api/token/me', views.issue_token, name='issue_token_api'),
+    path('api/save-speech', views.save_speech_result, name='save_speech_api'),
+
+    # остальное
+    path('', views.main_page, name='main'),
     path('translate/', views.translate_text, name='translate_text'),
-    path('about/', views.about, name='about'),  # Для страницы About
-    path('contact/', views.contact, name='contact'),  # Для страницы Contact
-    path('clear_chat/', views.clear_chat, name='clear_chat'),  # Очистка чата
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('clear_chat/', views.clear_chat, name='clear_chat'),
     path('token/me', views.issue_token, name='issue_token'),
     path('save-speech', views.save_speech_result, name='save_speech'),
 ]
